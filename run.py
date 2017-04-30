d854c3afd120bb90bb028b674a6ec5f9535d83f7
Jorn Tuyls <jorntuyls@jorns-mbp-3.home>
2017-04-30 10:58:54 +0200

    Scenario's in run files

--- run.py
+++ run.py
@@ -3,19 +3,29 @@ from data_loader import DataLoader
 from network_multiple_input import NetworkMI
 from network_pca import NetworkPCA
 
-scenario = 2
+scenario = 4
 
 data_loader = DataLoader('data/SemEval2016-Task3-CQA-QL-train-part1-subtaskA.xml',
                          'data/SemEval2016-Task3-CQA-QL-train-part2-subtaskA.xml',
                          'data/SemEval2016-Task3-CQA-QL-dev-subtaskA.xml')
 
+# Run feedforward neural network with multiple input
 if scenario == 1:
     network = NetworkMI(data_loader=data_loader)
     network.main(batch_size=32, num_epochs=50)
 
+# Run feedforward neural network with PCA preprocessor
 if scenario == 2:
     network = NetworkPCA(data_loader=data_loader)
     network.main(batch_size=32, num_epochs=50)
+
+# Run keras rnn on test input
+if scenario == 3:
+    import keras_rnn
+
+# Run keras rnn with PCA preprocessor
+if scenario == 4:
+    import keras_rnn_pca
 
 
 
--- run_pca.py
+++ run_pca.py
@@ -10,10 +10,12 @@ pca_a = PCA_analysis(data_loader)
 
 scenario = 3
 
+# Run PCA for visualization
 if scenario == 1:
     X_r, idx_r, X_v, idx_v = pca_a.run(n_components=2, downsampling=None)
     pca_a.visualize_pca_questions(X_r=X_r[:100], idx=idx_r[:100])
 
+# Run PCA with higher number of components
 if scenario == 2:
     X_r, idx_r, X_v, idx_v = pca_a.run(n_components=16, downsampling=None)
 
