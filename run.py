d407304f0c9ac01277ab87cf3bdab1a9a4eb4db0
Jorn Tuyls <jorntuyls@jorns-mbp-3.home>
2017-05-21 15:57:32 +0200

    Ne run.py with scenarios

--- run.py
+++ run.py
@@ -1,31 +1,88 @@
 
-from data_loader import DataLoader
-from network_multiple_input import NetworkMI
-from network_pca import NetworkPCA
+from main.data_loader import DataLoader
+from main.data_loader_pairwise import PairwiseDataLoader
+from main.ffnn_network_multiple_input import NetworkMI
+from main.ffnn_ranking_network import FFNNRankingNetwork
+from main.ffnn_ranking_network_sigmoid import FFNNRankingNetworkSigmoid
+from main.ffnn_network_pca import NetworkPCA
 
-scenario = 4
+from main.keras_rnn import KerasRNN
+from main.keras_rnn_ranking import KerasRNNRanking
+from main.keras_rnn_pca import KerasRNNPCA
+
+scenario = 6.0
 
 data_loader = DataLoader('data/SemEval2016-Task3-CQA-QL-train-part1-subtaskA.xml',
                          'data/SemEval2016-Task3-CQA-QL-train-part2-subtaskA.xml',
-                         'data/SemEval2016-Task3-CQA-QL-dev-subtaskA.xml')
+                         'data/SemEval2016-Task3-CQA-QL-dev-subtaskA.xml',
+                         'data/test_input.xml')
+data_loader_pairwise = PairwiseDataLoader('data/SemEval2016-Task3-CQA-QL-train-part1-subtaskA.xml',
+                                          'data/SemEval2016-Task3-CQA-QL-train-part2-subtaskA.xml',
+                                          'data/SemEval2016-Task3-CQA-QL-dev-subtaskA.xml',
+                                          'data/test_input.xml')
 
-# Run feedforward neural network with multiple input
-if scenario == 1:
+# 1. Run feedforward neural network with multiple input for classification
+# Run on test dataset
+if scenario == 1.0:
     network = NetworkMI(data_loader=data_loader)
-    network.main(batch_size=32, num_epochs=50)
+    network.main(batch_size=32, num_epochs=5, test=True)
+# Run on validation (dev) dataset
+if scenario == 1.1:
+    network = NetworkMI(data_loader=data_loader)
+    network.main(batch_size=32, num_epochs=50, dropout=0.3, test=False)
 
-# Run feedforward neural network with PCA preprocessor
-if scenario == 2:
+# 2. Run feedforward neural network with PCA preprocessor
+if scenario == 2.0:
     network = NetworkPCA(data_loader=data_loader)
     network.main(batch_size=32, num_epochs=50)
 
-# Run keras rnn on test input
-if scenario == 3:
-    import keras_rnn
+# 3. Run feedforward neural network with multiple input for learning to rank
+# Run network with softmax outputon test dataset
+if scenario == 3.0:
+    network = FFNNRankingNetwork(data_loader_pairwise=data_loader_pairwise)
+    network.main(batch_size=32, num_epochs=1, prediction_filename="scorer/scenario_3_0.pred",
+                 test=True, save_data_after_loading=True)
+# Run network with softmax output on validation (dev) dataset
+if scenario == 3.1:
+    network = FFNNRankingNetwork(data_loader_pairwise=data_loader_pairwise)
+    network.main(batch_size=32, num_epochs=1, dropout=0.3, loss="hinge", prediction_filename="scorer/scenario_3_1.pred")
+# Run network with sigmoid output on validation (dev) dataset
+if scenario == 3.2:
+    network = FFNNRankingNetworkSigmoid(data_loader_pairwise=data_loader_pairwise)
+    network.main(batch_size=32, num_epochs=50, prediction_filename="scorer/scenario_3_2.pred")
 
-# Run keras rnn with PCA preprocessor
-if scenario == 4:
-    import keras_rnn_pca
+
+# 4. Run keras recursive neural network for classification
+# Run on test dataset
+if scenario == 4.0:
+    keras_rnn = KerasRNN(data_loader=data_loader)
+    keras_rnn.main(embed_hidden_size=50, rnn_size=100, batch_size=32, num_epochs=40, test=True,
+                   prediction_filename="scorer/scenario_4_0.pred")
+# Run on validation (dev) dataset
+if scenario == 4.1:
+    keras_rnn = KerasRNN(data_loader=data_loader)
+    keras_rnn.main(embed_hidden_size=50, rnn_size=100, batch_size=32, num_epochs=1,
+                   prediction_filename="scorer/scenario_4_1.pred")
+
+# 5. Run keras rnn with PCA preprocessor
+if scenario == 5.0:
+    keras_rnn_pca = KerasRNNPCA(data_loader=data_loader)
+    keras_rnn_pca.main(embed_hidden_size=50, rnn_size=100, batch_size=32, num_epochs=1,
+                       prediction_filename="scorer/scenario_5_0.pred")
+
+# 6. Run keras recursive neural network
+# Run on test dataset
+if scenario == 6.0:
+    keras_rnn_ranking = KerasRNNRanking(data_loader_pairwise=data_loader_pairwise)
+    keras_rnn_ranking.main(embed_hidden_size=50, rnn_size=100, batch_size=32, num_epochs=1, test=True,
+                            prediction_filename="scorer/scenario_6_0.pred")
+# Run on validation (dev) dataset
+if scenario == 6.1:
+    keras_rnn_ranking = KerasRNNRanking(data_loader_pairwise=data_loader_pairwise)
+    keras_rnn_ranking.main(embed_hidden_size=50, rnn_size=100, batch_size=512, num_epochs=1,
+                            prediction_filename="scorer/scenario_6_1.pred")
+
+
 
 
 
